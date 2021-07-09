5f705e077592c14aa2b2e8bf74b7b0576c803f49
Sayed Abdul-Aziz <71665268+sayedabdul-aziz@users.noreply.github.com>
2021-07-09 17:00:04 +0200

    Add Update data JDBC Application

---
+++ Update/src/JDBC_Ex/ConnectClass.java
@@ -0,0 +1,25 @@
+
+package JDBC_Ex;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectClass {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 3306;
+    private static final String DB_NAME = "jdbc_course";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    private static final String URL = String.format("jdbc:mysql://%s:%d/%s",HOST,PORT,DB_NAME);
+    // URL = "jdbc:mysql://localhost:3306/jdbc_course"
+    static Connection con;
+    
+   public Connection getConnect() throws SQLException{
+       con=DriverManager.getConnection(URL, USER, PASS);
+       return con;
+   }
+    
+    
+}
